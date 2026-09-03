890435e75d6eef46809c28376382de94da8d7927
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-04 07:05:08 +0800

    Create day03.java to demonstrate escape sequences
    This Java program demonstrates the use of escape sequences for new lines, tabs, double quotation marks, and backslashes.

---
+++ day03.java
@@ -0,0 +1,19 @@
+public class day03 {
+    public static void main(String[] args) {
+	
+        // \n (New Line)
+        System.out.println("First line.\nSecond line.");
+        
+        // \t (Tab)
+        System.out.println("Nama\t:\tRhaifan Firansyah");
+        System.out.println("NIM\t:\tD0226040");
+        System.out.println("Prodi\t:\tInformatika");
+        
+        // \" (Double Quotation Marks)
+        System.out.println("Suka berkata \"Bagus!\"");
+        
+        // \\ (Backslash)
+        System.out.println("Alamat File : \\Dokumen\\100DaysofCoding");
+        
+    }
+}
