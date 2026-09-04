7f8de7a05c5f17c2aceef2f4db2699dadd256c56
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-04 12:19:53 +0800

    Add day03.java with student profile output

---
+++ day03.java
@@ -0,0 +1,19 @@
+public class day03 {
+    public static void main(String[] args) {
+	
+        // \n (New Line)
+        System.out.println("Profil Mahasiswa \nNama    : Rhaifan Firansyah");
+        
+        // \t (tab)
+        System.out.print("NIM\t: D0226040\n");
+        System.out.print("Prodi\t: Informatika\n");
+        System.out.print("Alamat\t: Cilallang\n");
+        
+        // \" (double quotation marks)
+        System.out.println("\"Mahasiswa Unsulbar\"");
+        
+        // \\ (backslash)
+        System.out.println("Alamat file :\\dokumen\\100DaysofCoding");
+        
+    }
+}
