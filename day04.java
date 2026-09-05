03315afdaa23d031733085612aef3b7e97492d3a
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-05 12:31:12 +0800

    Add day04.java with basic data type examples

---
+++ day04.java
@@ -0,0 +1,33 @@
+public class day04 {
+    
+     public static void main(String[] args) {
+         
+         byte panjangkabel = 30;
+         /* byte 
+          * Uses: Stores very small numbers to save memory.
+          */
+         
+         System.out.println("Panjang kabel Komp A ke Komp B  : " + panjangkabel +"cm");
+         
+         short hargabensin = 10000;
+         /* short
+          * Uses: Stores small numerical values.
+          */
+         System.out.println("Harga bensin  : "+"Rp."+ hargabensin);
+          
+         int jarakrumahkeunsulbar = 1500000;
+         /* int
+          * Uses: Most frequently and commonly used for integers.
+          */
+         System.out.println("Jarak Rumah ke Unsulbar  : "+ jarakrumahkeunsulbar +" cm");
+          
+         long luasbumi = 510072000;
+         /* long
+          * Use: Stores very large numerical values.
+          */
+         
+         System.out.println("Luas Bumi  : "+ luasbumi +" km²");
+        
+    }
+    
+}
