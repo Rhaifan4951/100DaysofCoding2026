8a171bcc0eea0dcd8d2c458f849df7fc1438b5e7
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-08 07:47:50 +0800

    Add student biodata display in day07.java

---
+++ day07.java
@@ -0,0 +1,28 @@
+public class day07 {
+    public static void main(String [] args){
+        
+        /*String
+         * String digunakan untuk menyimpan teks atau kumpulan karakter.
+         */
+         
+        String Nama ="Rhaifan Firansyah";
+        String nim = "D0226040";
+        String fakultas = "Teknik";
+        String prodi ="Informatika";
+        String kelas = "Informatika D";
+        String alamat = "Majene";
+        String hobi = "Bermain Game";
+        
+        System.out.println("=================\nBiodata Mahasiswa");
+        System.out.println("=================\n  ");
+        System.out.println("Nama\t: "+ Nama);
+        System.out.println("NIM\t: "+ nim);
+        System.out.println("Fakultas: "+ fakultas);
+        System.out.println("Prodi\t: "+ prodi);
+        System.out.println("Kelas\t: "+ kelas);
+        System.out.println("Alamat\t: "+ alamat);
+        System.out.println("Hobi\t: "+ hobi);
+        
+    }
+    
+}
