50a71ace37351284c0835845909f355dea0e3c7c
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-07 17:13:26 +0800

    Add day06.java with student data display

---
+++ day06.java
@@ -0,0 +1,26 @@
+public class day06 {
+    public static void main(String[] args){
+        
+        String Nama = "Rhaifan Firansyah";
+        
+        /* Karakter (char)
+         * Tipe data char digunakan untuk menyimpan satu karakter 
+         * atau huruf tunggal yang diapit oleh tanda petik tunggal 
+         */
+        
+        char Golongandarah ='B';
+        
+        /* Boolean (boolean)
+         * Tipe data boolean digunakan untuk menyimpan nilai kebenaran dalam logika pemrograman, yaitu benar atau salah.
+         */
+         
+        boolean Pelajar = true;
+        
+        System.out.println("==============\nDATA MAHASISWA\n==============\n  ");
+        System.out.println("Nama\t\t: " + Nama);
+        System.out.println("Gol.Darah\t: " + Golongandarah);
+        System.out.println("Pelajar\t\t: " + Pelajar);
+        
+        
+    }
+}
