8a3e58bdd8b07a7cc6aa5d13634235d7fc819606
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-09 19:49:30 +0800

    Add day08.java with variable declaration and update

---
+++ day08.java
@@ -0,0 +1,31 @@
+public class day08 {
+      public static void main(String [] args) {
+    
+       /* Deklarasi
+        * Deklarasi adalah proses mengenalkan variabel baru ke program.
+        */
+    
+          String nama;
+          int umur;
+          double tinggibadan; 
+          
+       /* Inisialisasi
+        * Inisialisasi adalah proses pemberian nilai pertama kali kepada variabel yang sudah dideklarasikan.
+        */
+          
+          nama ="Rhaifan Firansyah";
+          umur = 18;
+          tinggibadan = 163.5;
+          
+          System.out.println("Nama\t\t: " + nama);
+          System.out.println("Umur\t\t: " + umur);
+          System.out.println("Tingga badan\t: " + tinggibadan + " \n");
+          
+        /* Update
+         * Update adalah proses mengubah nilai yang sudah ada di dalam variabel dengan nilai yang baru.
+         */
+          
+         umur = 19;
+          System.out.println("Umur setelah update : " + umur);
+    }
+}
