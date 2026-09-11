545fa5f8ca4b5ad29e549bb1002f700669939df8
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-11 18:07:37 +0800

    Display min and max values for Java data types
    Prints the minimum and maximum values for various data types in Java.

---
+++ day10.java
@@ -0,0 +1,40 @@
+public class day10 {
+    public static void main(String[] args){
+        
+        // Batas minimal dan maksimal type data byte
+        System.out.println("BYTE");
+        System.out.println("Minimum : " + Byte.MIN_VALUE);
+        System.out.println("Maximum : " + Byte.MAX_VALUE);
+        
+        // Batas minimal dan maksimal type data short
+        System.out.println("\nSHORT");
+        System.out.println("Minimum : " + Short.MIN_VALUE);
+        System.out.println("Maximum : " + Short.MAX_VALUE);
+        
+        // Batas minimal dan maksimal type data int
+        System.out.println("\nINT");
+        System.out.println("Minimum : " + Integer.MIN_VALUE);
+        System.out.println("Maximum : " + Integer.MAX_VALUE);
+        
+        // Batas minimal dan maksimal type data long
+        System.out.println("\nLONG");
+        System.out.println("Minimum : " + Long.MIN_VALUE);
+        System.out.println("Maximum : " + Long.MAX_VALUE);
+        
+        // Batas minimal dan maksimal type data double
+        System.out.println("\nDOUBLE");
+        System.out.println("Minimum : " + Double.MIN_VALUE);
+        System.out.println("Maximum : " + Double.MAX_VALUE);
+        
+        // Batas minimal dan maksimal type data float
+        System.out.println("\nFLOAT");
+        System.out.println("Minimum : " + Float.MIN_VALUE);
+        System.out.println("Maximum : " + Float.MAX_VALUE);
+        
+        // Batas minimal dan maksimal type data char
+        System.out.println("\nCHAR");
+        System.out.println("Minimum : " + (int)Character.MIN_VALUE);
+        System.out.println("Maximum : " + (int)Character.MAX_VALUE);
+        
+     }
+}
