4233fbdec53b14e0a53e18d1e767118d669c1657
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-16 22:49:07 +0800

    Refactor variable names for clarity

--- day15.java
+++ day15.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
         Scanner in = new Scanner(System.in);
         
         System.out.print("Masukkan Harga Barang\t: ");
-        int h = in.nextInt();
+        int hargaBarang = in.nextInt();
         System.out.print("Masukkan Jumlah Barang\t: ");
-        int j = in.nextInt();
+        int jumlahBarang = in.nextInt();
         System.out.print("Masukkan Jumlah Orang\t: ");
-        int o = in.nextInt();
+        int jumlahOrang = in.nextInt();
         
-        int k = h * j;
-        int m = k / o;
-        System.out.println("\nTotal harga\t: " + k);
-        System.out.println("Harga per Orang\t: " + m);
+        int totalHarga = hargaBarang * jumlahBarang;
+        int hargaPerOrang = totalHarga / jumlahOrang;
+        System.out.println("\nTotal harga\t: " + totalHarga);
+        System.out.println("Harga per Orang\t: " + hargaPerOrang);
      }
   }
