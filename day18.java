32047534f433fc3189bc2f5b0b2c3ce711ed9d3c
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-19 18:10:35 +0800

    Add day18 class to demonstrate type casting

---
+++ day18.java
@@ -0,0 +1,20 @@
+public class day18 {
+    public static void main(String[] args) {
+        
+        byte nilaiByte = 15;
+        short nilaiShort = nilaiByte;
+        int nilaiInt = nilaiShort;
+        long nilaiLong = nilaiInt;
+        float nilaiFloat = nilaiLong;
+        double nilaiDouble = nilaiFloat;
+        
+        System.out.println("Byte\t: " + nilaiByte);
+        System.out.println("Short\t: " + nilaiShort);
+        System.out.println("Int\t: " + nilaiInt);
+        System.out.println("Long\t: " + nilaiLong);
+        System.out.println("Float\t: " + nilaiFloat);
+        System.out.println("Double\t: " + nilaiDouble);
+      
+    }
+
+}
