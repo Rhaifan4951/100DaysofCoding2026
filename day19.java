61bf974dbb66f230cc45a739b8198bdbfc9178ab
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-20 19:44:42 +0800

    Add day19 class with type casting examples

---
+++ day19.java
@@ -0,0 +1,20 @@
+public class day19 {
+    public static void main(String[] args){
+        
+        double nilaiDouble = 123.45;
+        float nilaiFloat = (float) nilaiDouble;
+        long nilaiLong = (long) nilaiFloat;
+        int nilaiInt = (int) nilaiLong;
+        short nilaiShort = (short) nilaiInt; 
+        byte nilaiByte = (byte) nilaiShort;
+        
+        System.out.println("Nilai Double\t: " + nilaiDouble);
+        System.out.println("Nilai Float\t: " + nilaiFloat);
+        System.out.println("Nilai long\t: " + nilaiLong);
+        System.out.println("Nilai Int\t: " + nilaiInt);
+        System.out.println("Nilai Short\t: " + nilaiShort);
+        System.out.println("Nilai Byte\t: " + nilaiByte);
+        
+        
+    }
+}
