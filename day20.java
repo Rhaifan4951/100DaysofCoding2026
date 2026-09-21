ccc237f587c019834788dc54863f3c08f3e4eeeb
Rhaifan4951 <budiartisinta1@gmail.com>
2026-09-21 19:47:23 +0800

    Add day20 class with main method and variable outputs

---
+++ day20.java
@@ -0,0 +1,21 @@
+public class day20 {
+    public static void main(String[] args) {
+	    
+        int angka = 505;
+        double nilai = 99.5;
+        char huruf = 'S';
+        boolean status = true;
+        
+        String angkaString = String.valueOf(angka);
+        String nilaiString = String.valueOf(nilai);
+        String hurufString = String.valueOf(huruf);
+        String statusString = String.valueOf(status);
+        
+        System.out.println("Angka\t: " + angkaString);
+        System.out.println("Nilai\t: " + nilaiString);
+        System.out.println("Huruf\t: " + hurufString);
+        System.out.println("Status\t: " + statusString);
+
+   }
+    
+}
